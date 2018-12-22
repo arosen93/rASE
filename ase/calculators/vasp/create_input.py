7327cb8041f19a630e96afae864836162bdb1679
Andrew Rosen <rosen@u.northwestern.edu>
2018-12-22 17:12:10 -0500

    Don't overwrite LMAXMIX if provided

--- ase/calculators/vasp/create_input.py
+++ ase/calculators/vasp/create_input.py
@@ -1195,19 +1195,20 @@ class GenerateVaspInput(object):
         syms = np.unique(atoms.get_atomic_numbers()).tolist()
         d_block = np.arange(21,31,1).tolist()+np.arange(39,49,1).tolist()+np.arange(72,81,1).tolist()+np .arange(104,113,1).tolist()
         f_block = np.arange(57,72,1).tolist()+np.arange(89,104,1).tolist()
-        d_el = False
-        f_el = False
-        for sym in syms:
-        	if sym in d_block:
-        		d_el = True
-        	elif sym in f_block:
-        		f_el = True
-        		d_el = False
-        		break
-        if d_el:
-        	self.int_params['LMAXMIX'] = 4
-        elif f_el:
-        	self.int_params['LMAXMIX'] = 6
+        if self.int_params['LMAXMIX'] is None:
+	        d_el = False
+	        f_el = False
+	        for sym in syms:
+	        	if sym in d_block:
+	        		d_el = True
+	        	elif sym in f_block:
+	        		f_el = True
+	        		d_el = False
+	        		break
+	        if d_el:
+	        	self.int_params['LMAXMIX'] = 4
+	        elif f_el:
+	        	self.int_params['LMAXMIX'] = 6
         magmom_written = False
         incar = open(join(directory, 'INCAR'), 'w')
         incar.write('INCAR created by Atomic Simulation Environment\n')
